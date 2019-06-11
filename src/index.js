84452dd9875666d76749cd290adca18268e11665
NardaC <nardac6@gmail.com>
2019-06-11 16:23:27 -0500

    test %50

--- src/index.js
+++ src/index.js
@@ -1,21 +1,66 @@
-const path = require('path');
+import path from 'path';
+import  fs from 'fs';
+//const fspromises = require('fs.promises')
+
+const ruta1 = '/Users/narda/Desktop/Lim009/LIM009-fe-md-links/src'
 const ruta3 = '‎⁨/Users/narda/Desktop/';
 let ruta2 = 'index.js';
 
-const isPathAbsolute = (ruta) => {
+export const isPathAbsolute = (ruta) => {
     let absolute = path.isAbsolute(ruta);
     if(absolute){
         return absolute;
-        //console.log("es absoluta");
     } else {
         return path.resolve(ruta);
-        //console.log("no es absoluta");
     }
     //return absolute
 }
 
-module.exports = {
-    isPathAbsolute
-}
+// function filePathExists(filePath) {
+//     return new Promise((resolve, reject) => {
+//       fs.stat(filePath, (err, stats) => {
+//         if (err && err.code === 'ENOENT') {
+//           return resolve(false);
+//         } else if (err) {
+//           return reject(err);
+//         }
+//         if (stats.isFile() || stats.isDirectory()) {
+//           return resolve(true);
+//         }
+//       });
+//     });
+//   }
+//    console.log(filePathExists(ruta1));
 
-console.log(isPathAbsolute(ruta2));
+export const isFile = (ruta)=>{
+    const stats = fs.statSync(ruta);
+    return stats.isFile();
+}
+//console.log(isFile('holi'));
+
+
+export const isDirectory = (ruta)=>{
+    const stats = fs.statSync(ruta);
+    return stats.isDirectory()
+}
+ //console.log(isFile(ruta2));
+// console.log(isDirectory(ruta2));
+// console.log(isDirectory(ruta1));
+
+export const readFile =(ruta) =>{
+    let archivo = fs.readFileSync(ruta, 'utf-8');
+    return archivo
+}
+//console.log(readFile(ruta2));
+
+export const readDir =(ruta) =>{
+    let carpeta = fs.readdirSync(ruta, 'utf-8');
+    return carpeta
+}
+//console.log(readDir(ruta1));
+
+export const isMd = (str) => {
+    let md = path.extname(str) === '.md'
+    return md
+}
+//console.log(isMd('prueba.md'));
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,4 @@
-const { isPathAbsolute } = require("../lib/index.js");
+import { isPathAbsolute, isFile, isDirectory } from "../src/index.js";
 
 describe('funcion que indica si la ruta es absoluta',() => {
     it('deberia ser una funcion',() => {
@@ -9,5 +9,43 @@ describe('funcion que indica si la ruta es absoluta',() => {
     })
     it('deberia retornar una ruta absoluta si es relativa',() => {
         expect(isPathAbsolute('index.js')).toBe('/Users/narda/Desktop/Lim009/LIM009-fe-md-links/index.js');
+    })
+});
+
+describe('funcion que indica si es archivo',() => {
+    it('deberia ser una funcion', () => {
+        expect(typeof isFile).toBe('function');
+    })
+    it('deberia retornar true si es un archivo',() => {
+        expect(isFile('src/index.js')).toBe(true);
+    })
+    it('deberia retornar false si no es un archivo',() => {
+        expect(isFile('src/')).toBe(false);
+    })
+    it('deberia fallar si la ruta no existe',() => {
+        try {
+            isFile('holi')
+        } catch(err) {
+            expect(err.code).toBe('ENOENT');
+        }        
+    })
+})
+
+describe('funcion que indica si es una carpeta',() => {
+    it('deberia ser una funcion', () => {
+        expect(typeof isDirectory).toBe('function');
+    })
+    it('deberia retornar true si es una carpeta',() => {
+        expect(isDirectory('src/')).toBe(true);
+    })
+    it('deberia retornar false si no es una carpeta',() => {
+        expect(isDirectory('src/index.js')).toBe(false);
+    })
+    it('deberia fallar si la carpeta no existe',() => {
+        try {
+            isDirectory('holi')
+        } catch(err) {
+            expect(err.code).toBe('ENOENT');
+        }        
     })
 })
